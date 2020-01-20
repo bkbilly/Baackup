1a705bd1b4cb760c599023af18d766a4b1c14c2b
bkbilly <bkbilly@hotmail.com>
2020-01-20 15:43:44 +0000

    added recuring process

--- webinterface/apps.py
+++ webinterface/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
+from apscheduler.schedulers.background import BackgroundScheduler
+from . import tasks
 
 
 class WebinterfaceConfig(AppConfig):
     name = 'webinterface'
+    def ready(self):
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(tasks.update_forecast, 'interval', seconds=5)
+        scheduler.start()
+        print('----- started tasks')
