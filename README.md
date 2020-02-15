9aa9a47909ea1c5b8ed19cad6964467ea4a6b680
bkbilly <bkbilly@hotmail.com>
2020-02-15 21:57:34 +0000

    changed default url

---
+++ README.md
@@ -0,0 +1,5 @@
+```bash
+sudo pip3 install -r requirements.txt
+python3 manage.py migrate
+python3 manage.py runserver
+```
--- baaackup/urls.py
+++ baaackup/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('webinterface/', include('webinterface.urls')),
+    path('', include('webinterface.urls')),
     path('admin/', admin.site.urls),
 ]
