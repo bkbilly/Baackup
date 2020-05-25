f7e855de3adfc3de2e5344e0f6bf2177c9a46442
bkbilly <bkbilly@hotmail.com>
2020-05-25 03:10:24 +0100

    Remove debuging

--- baaackup/settings.py
+++ baaackup/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'le!t)1bhh0ce$c@lmahc&vt&=_8qie8d5=jkmw_hltfbn!nr$6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
